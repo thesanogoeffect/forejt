801c9acbe9ea04d6f23a96d0a187ad3712dc7eee
Jakub <jakubwerner1@gmail.com>
2023-09-30 21:02:43 +0200

    formatting hotfix

--- forejt-landing-page/src/layouts/default/DefaultView.vue
+++ forejt-landing-page/src/layouts/default/DefaultView.vue
@@ -7,6 +7,7 @@
     <v-col class="text-center py-2" cols="12">
       made with ❤️ by <a href="https://www.linkedin.com/in/jakubwerner">Jakub Werner</a> <br />using data from
       <a href="https://www.psmf.cz">psmf.cz</a>
+      <br />
       <a href="mailto:jakubwerner1@gmail.com">Contact</a>
 
      
