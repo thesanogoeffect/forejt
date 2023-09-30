29ee6385467378ee065719a1f94f76362cc48a68
Jakub <jakubwerner1@gmail.com>
2023-09-30 12:12:26 +0200

    test commit

--- forejt-landing-page/src/views/Home.vue
+++ forejt-landing-page/src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <v-app>
     <v-main>
+      <v-container class="text-center py-5"> 
+        <h1 class="display-1">Forejt</h1>
+      </v-container>
       <v-container fluid>
         <v-row align="center" justify="center">
-          <v-col cols="12" md="4">
-            <v-card>
-              <v-card-title> Custom iCal Variants </v-card-title>
-              <v-card-actions>
+          <v-col cols="12" md="6" lg="4">
+            <v-card class="mx-auto mb-5">
+              <v-card-title class="text-center">
+                <span class="headline">Custom iCal Variants</span>
+              </v-card-title>
+              <v-card-actions class="justify-center">
                 <a :href="appleLink">
                   <v-btn color="primary">Apple</v-btn>
                 </a>
@@ -25,8 +30,14 @@
         </v-row>
       </v-container>
     </v-main>
+    <v-footer fixed padless>
+      <v-col class="text-center py-2" cols="12">
+        made with ❤️, by Jakub Werner
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
+
 
 <script>
 export default {
