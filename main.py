92011c85e48a9524dd16dace5007ca66eeae9a72
Jakub Werner <jakubwerner1@gmail.com>
2024-03-04 13:44:11 +0100

    fixed datetime bug in main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pytz import timezone
 from icalendar import Calendar, Event
 import logging
 
-PRESEASON = datetime.datetime.now() < datetime.datetime(2024, 3, 18)
+PRESEASON = datetime.now() < datetime(2024, 3, 18)
 tz = timezone("Europe/Prague")
 email_list = set()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
