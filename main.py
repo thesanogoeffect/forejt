ef12b6a2a45adde5f87c8f40a6bf168b4688e8ff
Jakub Werner <jakubwerner1@gmail.com>
2024-07-18 10:32:20 +0200

    updated preseason date for Autumn 2024

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pytz import timezone
 from icalendar import Calendar, Event
 import logging
 
-PRESEASON = datetime.now() < datetime(2024, 3, 18)
+PRESEASON = datetime.now() < datetime(2024, 9, 1)
 tz = timezone("Europe/Prague")
 email_list = set()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
